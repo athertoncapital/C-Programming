431077fc5b772129e19a65b75704b4ed444fac75
wchuanghard <wchuang@g.harvard.edu>
2016-03-16 17:01:37 -0700

    update

---
+++ 0047_namespace_04.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "The Planck constant has";
+  cout << " dimensions of physical action;";
+  cout << "these are the same as those of angular momentum,";
+  cout << "i.e., energy multiplied by time, ";
+  cout << "or momentum multiplied by distance. ";
+  return 0;
+} 
---
+++ 004A_cout_07.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int number;
+
+  number = 5;
+  cout << "The value in number is " << "number" << endl;
+  cout << "The value in number is " << number << endl;
+  cout << "Today we sold " << number << " bushels of apples.\n";
+  return 0;
+}
