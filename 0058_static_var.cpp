184b56ba6a970e0db00dfe1127d591c808aed70e
wchuanghard <wchuang@g.harvard.edu>
2016-03-19 00:59:46 -0700

    update

---
+++ 0058_static_var.cpp
@@ -0,0 +1,32 @@
+// Static local variables are only initialized once.
+#include <iostream>
+using namespace std;
+
+void printStatic(); 
+void print();
+
+int main()
+{
+
+  for (int count = 0; count < 5; count++)
+    printStatic();
+  for (int count = 0; count < 5; count++)
+    print();
+  for (int count = 0; count < 5; count++)
+    printconst();
+  return 0;
+}
+void print()
+{
+  int Num = 10;
+  cout << "Num is " << Num << endl;
+  Num++;
+}
+
+void printStatic()
+{
+  static int statNum = 5;
+
+  cout << "statNum is " << statNum << endl;
+  statNum++;
+} 
