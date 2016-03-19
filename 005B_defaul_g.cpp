e1f6e210fdafbf0b7a61467f4cb5738dde00490e
wchuanghard <wchuang@g.harvard.edu>
2016-03-19 01:10:50 -0700

    update

---
+++ 005B_defaul_g.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int globalNum;  // Global variable
+
+int main()
+{
+  int loc;
+  cout << "globalNum is " << globalNum << endl;
+  cout << "lcaal Num is " << loc << endl;
+  return 0;
+}
