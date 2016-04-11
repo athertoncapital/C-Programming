3e1c276fa7fee4c78f60beb52a8c2cd47e415fa3
wchuanghard <wchuang@g.harvard.edu>
2016-04-10 17:21:56 -0700

    update readme

--- README.d
+++ README.d
@@ -5,3 +5,9 @@ Reference:
 
 [2] An Introduction to Parallel Programming by Peter Pacheco
 https://www.cs.usfca.edu/~peter/ipp/
+
+[3] Galles, David. "Modern Compiler Design."
+http://www.cs.usfca.edu/~galles/compilerdesign/
+
+[4] Pang, Tao. "An Introduction to Computational Physics."
+http://www.physics.unlv.edu/~pang/cp.html
