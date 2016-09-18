71f1fe0dc852db633717ca44a0cf6550c709d8b0
William_Chuang <wchuang@g.harvard.edu>
2016-09-18 12:41:20 -0700

    Update README.d

--- README.d
+++ README.d
@@ -1,12 +1,12 @@
-Implementations of Game Engineering, Algorithms, Compilers, and Parallel Programming.
+#Implementations of Game Engineering, Algorithms, Compilers, and Parallel Programming.
 
-Reference:
+##Reference:
 [1] http://www.cplusplus.com/reference/
 
 [2] An Introduction to Parallel Programming by Peter Pacheco
 https://www.cs.usfca.edu/~peter/ipp/
 
-[3] Galles, David. "Modern Compiler Design."
+###[3] Galles, David. "Modern Compiler Design."
 http://www.cs.usfca.edu/~galles/compilerdesign/
 
 [4] Pang, Tao. "An Introduction to Computational Physics."
